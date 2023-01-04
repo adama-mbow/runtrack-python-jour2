24c8bea314ee18df10b44352eea65ecb635d3737
adama <adama.mbow@laplateforme.io>
2023-01-04 10:33:38 +0100

    fonction

--- jour2/plus_loin/main.py
+++ jour2/plus_loin/main.py
@@ -5,11 +5,11 @@ def triangle(a,b,c):
         print("on ne peut pas construire une triangle aves ces longueurs.")
     if (a==b) and (b==c):
         return("on a une triangle équilaterale")
-    elif ((a == b) or (b == a) or (c == a)) and ((a*a + b*b ==round (c*c)) or (b*b +c*c == round(a*a)) or (c*c + a*a == round(b*b))):
+    elif ((a == b) or (b == a) or (c == a)) and ((a*a + b*b ==round(c*c)) or (b*b +c*c == round(a*a)) or (c*c + a*a == round(b*b))):
         return("on a une triangle rectangle et isocèle")
     elif (a == b) or (b == a) or (c == a):
         return ("on a une triangle isocele")
-    elif (a*a + b*b ==round (c*c)) or (b*b +c*c ==round (a*a)) or (c*c + a*a == round(b*b)):
+    elif (a*a + b*b ==round(c*c)) or (b*b +c*c ==round(a*a)) or (c*c + a*a == round(b*b)):
         return("on a une triangle rectangle")
     else:
         return("on a une triangle quelconque")
